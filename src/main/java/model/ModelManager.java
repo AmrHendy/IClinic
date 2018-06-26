d595f820f70cb3e7e16487524969a6d20f3f304f
Amr Hendy <amr.m.hendy@gmail.com>
2018-06-26 13:28:42 +0200

    adding ModelManager

--- src/main/java/model/ModelManager.java
+++ src/main/java/model/ModelManager.java
@@ -1,4 +1,51 @@
 package main.java.model;
 
+import java.sql.*;
+
 public class ModelManager {
+    private static String url = "jdbc:mysql://localhost:3306/iclinic";
+    private static String user = "root";
+    private static String pass = "root12_";
+    private static ModelManager model;
+    private Connection connection;
+
+    private ModelManager() {
+        startConnection();
+    }
+
+    public static synchronized ModelManager getInstance()
+    {
+        if (model == null) {
+            model = new ModelManager();
+        }
+        return model;
+    }
+
+    public void startConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(query);
+        return statement.getResultSet();
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
